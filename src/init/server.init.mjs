c0dc7b158925bd06951847843040db1e2ae2e773
Devemvt <devemvt@gmail.com>
2024-05-06 22:25:05 -0600

    Se llama al servicio del login desde que se arranca el servidor

--- src/init/server.init.mjs
+++ src/init/server.init.mjs
@@ -1,15 +1,29 @@
-import app from "../configs/server.config.mjs";
-import loggerService from "../services/logger.service.mjs";
+import app from '../configs/server.config.mjs';
+import loggerService from '../services/logger.service.mjs';
+import dotenv from 'dotenv';
+import LoginService from '../services/login.service.mjs';
+
+dotenv.config();
+
+// TODO: Quitar de aqui y colocarlo en un archivo init.
+const loginService = new LoginService(
+	process.env.VENTIAPP_USERNAME,
+	process.env.VENTIAPP_PASSWORD,
+	process.env.VENTIAPP_GRANT_TYPE
+);
+
+loginService.inicarSesion();
+
+// TODO: Implementar un modo producción y un modo desarrollo.
 
 /**
  * Iniciar el servidor en modo de desarrollo.
  * 
  * @callback
- * TODO: Implementar el archivo .env para obtner de ahi el puerto, asi como definir los modos.
 **/
-app.listen( 4000 , () => {
+app.listen( process.env.SV_PORT , () => {
 	loggerService.escribirLog(
 		'watcher',
-		`Aplicacion corriendo en el puerto: 4000\nModo: Dev`
+		`Aplicacion corriendo en el puerto: ${process.env.SV_PORT}\nModo: Dev`
 	);
 } );
