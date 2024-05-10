2afeed5a08e8a0b7217f907ddbf73ec8b3eb7d9a
Devemvt <devemvt@gmail.com>
2024-05-09 22:54:24 -0600

    Todo completado, se la obtencion de token del server init

--- src/init/server.init.mjs
+++ src/init/server.init.mjs
@@ -1,21 +1,12 @@
 import app from '../configs/server.config.mjs';
 import loggerService from '../services/logger.service.mjs';
 import dotenv from 'dotenv';
-import LoginService from '../services/login.service.mjs';
+import InicializarService from './inicializar.init.mjs';
 
+// Cargar la configuración del archivo de variables.
 dotenv.config();
 
-// TODO: Quitar de aqui y colocarlo en un archivo init.
-const loginService = new LoginService(
-	process.env.VENTIAPP_USERNAME,
-	process.env.VENTIAPP_PASSWORD,
-	process.env.VENTIAPP_GRANT_TYPE
-);
-
-loginService.inicarSesion();
-
 // TODO: Implementar un modo producción y un modo desarrollo.
-
 /**
  * Iniciar el servidor en modo de desarrollo.
  * 
