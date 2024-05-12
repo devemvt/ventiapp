81493c4ac45bc760ca5cbe87dc3f0552b54da9ad
Devemvt <devemvt@gmail.com>
2024-05-12 11:21:13 -0600

    Se agregan todas las rutas de categorias al router

--- src/routes/categorias/categoria.route.mjs
+++ src/routes/categorias/categoria.route.mjs
@@ -1,3 +1,9 @@
+import { actualizarRouter } from './actualizar.route.mjs';
+import { buscarRouter } from './buscar.route.mjs';
+import { crearRouter } from './crear.route.mjs';
+import { eliminarRouter } from './eliminar.route.mjs';
+import { mapearRouter } from './mapear.route.mjs';
+import { obtenerRouter } from './obtener.route.mjs';
 import express from 'express';
 
 /**
@@ -6,6 +12,66 @@ import express from 'express';
  * @type {express.Router} 
 **/
 const router = express.Router();
+
+/**
+ * Agregar el router de actualizar categoría en la ruta '/actualizar'.
+ * 
+ * @name actualizarRouter
+ * @memberof categoria
+ * @param {string} path - Ruta de la solicitud.
+ * @param {router} route - Router a utilizar.
+**/
+router.use( '/actualizar', actualizarRouter );
+
+/**
+ * Agregar el router de buscar categoría en la ruta '/buscar'.
+ * 
+ * @name buscarRouter
+ * @memberof categoria
+ * @param {string} path - Ruta de la solicitud.
+ * @param {router} route - Router a utilizar.
+**/
+router.use( '/buscar', buscarRouter );
+
+/**
+ * Agregar el router de crear categorías en la ruta '/crear'.
+ * 
+ * @name crearRouter
+ * @memberof categoria
+ * @param {string} path - Ruta de la solicitud.
+ * @param {router} route - Router a utilizar.
+**/
+router.use( '/crear', crearRouter );
+
+/**
+ * Agregar el router de eliminar categoría en la ruta '/eliminar'.
+ * 
+ * @name eliminarRouter
+ * @memberof categoria
+ * @param {string} path - Ruta de la solicitud.
+ * @param {router} route - Router a utilizar.
+**/
+router.use( '/eliminar', eliminarRouter );
+
+/**
+ * Agregar el router de mapear categoría en la ruta '/mapear'.
+ * 
+ * @name mapearRouter
+ * @memberof categoria
+ * @param {string} path - Ruta de la solicitud.
+ * @param {router} route - Router a utilizar.
+**/
+router.use( '/mapear', mapearRouter );
+
+/**
+ * Agregar el router de obtener categorías en la ruta '/obtener'.
+ * 
+ * @name obtenerRouter
+ * @memberof categoria
+ * @param {string} path - Ruta de la solicitud.
+ * @param {router} route - Router a utilizar.
+**/
+router.use( '/obtener', obtenerRouter );
 
 /**
  * Exporta el router como `categoriaRouter`.
